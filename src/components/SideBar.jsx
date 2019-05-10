b9cd5db9eb807311c84850e0f0ebb19c7a694af5
Victoria Martinez <vmartinez72@live.com>
2019-05-10 15:52:23 -0700

    add Cards Component

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -5,15 +5,29 @@ function Sidebar(){
   return(
     <div>
     <style jsx>{`
-
+      .whiteSquare{
+        position: absolute;
+        width: 302px;
+        height: 500px;
+        left: 52px;
+        top: 69px;
+        background: #FFFFFF;
+        border-radius: 10px;
+      }
       `}</style>
+      <div className="whiteSquare"></div>
       <Link to="/" style={{ textDecoration: 'none' }}>
-        <a>Kegs</a>
+        <a>Home</a>
       </Link>
-      <Link to="/about" style={{ textDecoration: 'none' }}>
-        <a>About</a>
+      <Link to="/form" style={{ textDecoration: 'none' }}>
+        <a>Form</a>
       </Link>
-      <Link to="/newKeg" style={{ textDecoration: 'none' }}>
+      <Link to="/wine-notes" style={{ textDecoration: 'none' }}>
+        <a>Notes</a>
+      </Link>
+      <Link to="/wine-pairings" style={{ textDecoration: 'none' }}>
+        <a>Pairings</a>
+      </Link>
     </div>
   )
 }
