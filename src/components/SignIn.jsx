08d36fd5d36e4ae4dc0d05f20ea8bd728887e35d
Victoria Martinez <vmartinez72@live.com>
2019-05-10 12:40:29 -0700

    add input fields

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -1,9 +1,17 @@
 import React from 'react';
+import background from '../assets/images/6.png';
 
 
 function SignIn(){
   return(
-
+    <div>
+    <img className="background2" src={background} alt="background picture of barrel, green grapes and wine bottles"></img>
+    <h4>user one</h4>
+      <input type="text" className="firstName"   placeholder="Enter first name">
+      <input type="text" className="lastName"   placeholder="Enter last name">
+      <input type="email" className="email"   placeholder="Enter email">
+      <input type="email" className="confirm-email"   placeholder="Confirm email">
+    </div>
   )
 }
 export default SignIn;
