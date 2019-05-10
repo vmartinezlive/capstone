3c423d81a9bcece814d15fb026e0bbf26ec0e473
Victoria Martinez <vmartinez72@live.com>
2019-05-10 13:39:36 -0700

    add content to App component

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,7 @@
 import React, { Component } from 'react';
+import LandingPage from 'LandingPage';
 import SignIn from 'SignIn';
+import background from '../assets/images/app.png';
 
 
 class App extends Components{
@@ -7,8 +9,29 @@ class App extends Components{
   render(){
     return(
       <div>
+        <style jsx>{`
+          .background{
+            position: absolute;
+            width: 1532px;
+            height: 1051px;
+            left: -92px;
+            top: -27px;
+          }
+          .logoDrawer{
+            position: absolute;
+            width: 260px;
+            height: 143px;
+            left: 1162px;
+            top: 39px;
+          }
+
+            `}</style>
+        <LandingPage/>
         <SignIn/>
+        <img className="background" src={background} alt="background picture of wine rack glasses and a large picture of green grapes"></img>
         
+
+
 
       </div>
 
